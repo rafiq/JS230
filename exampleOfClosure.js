159273733ccf420a36c1e77e0abae7d5cee9c28c
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2021-11-24 18:49:20 +0300

    Create exampleOfClosure.js

---
+++ exampleOfClosure.js
@@ -0,0 +1,37 @@
+var makeCounter = function() {
+    var privateCounter = 0;
+    function changeBy(val) {
+      privateCounter += val;
+    }
+    return {
+      increment: function() {
+        changeBy(1);
+      },
+
+      decrement: function() {
+        changeBy(-1);
+      },
+
+      value: function() {
+        return privateCounter;
+      }
+    }
+  };
+
+  var counter1 = makeCounter();
+  var counter2 = makeCounter();
+
+  alert(counter1.value());  // 0.
+
+  counter1.increment();
+  counter1.increment();
+  alert(counter1.value()); // 2.
+
+  counter1.decrement();
+  alert(counter1.value()); // 1.
+  alert(counter2.value()); // 0.
+//   Copy to Clipboard
+//   Notice how the two counters maintain their independence from one another. Each closure references a different version of the privateCounter variable through its own closure. Each time one of the counters is called, its lexical environment changes by changing the value of this variable. Changes to the variable value in one closure don't affect the value in the other closure.
+
+
+// Note: Using closures in this way provides benefits that are normally associated with object-oriented programming. In particular, data hiding and encapsulation.
