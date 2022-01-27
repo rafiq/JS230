8a9ba89c579bde17e357e21fa04d4d2368e9c9fe
Rafiq Kamal <71498824+Rafiq@users.noreply.github.com>
2022-01-27 19:02:07 +0300

    Update tracingDom.js

--- tracingDom.js
+++ tracingDom.js
@@ -1,3 +1,49 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function domTreeTracer(id, results = []){
+    let child = document.getElementById(id);
+    let parent = child.parentElement;
+    let siblings = parent.children;
+    let siblingNames = [...siblings].map(sibling => sibling.nodeName);
+
+    results.push(siblingNames);
+
+    return child.id === '1' ? results : domTreeTracer(parent.id, results);
+  };
+
+    // function domTreeTracer(id) {
+    //   let siblingsArr = [];
+    //   let currEl = document.getElementById(String(id))
+    //   while (currEl) {
+    //     let temp = [];
+    //     let siblings = currEl.parentNode.firstChild
+
+    //     while (siblings) {
+    //       if (siblings.nodeType === 1) {
+    //         temp.push(siblings.tagName)
+    //       }
+    //       siblings = siblings.nextSibling
+    //     }
+
+    //     siblingsArr.push(temp)
+
+    //     if (currEl.parentNode.nodeType === 1 && currEl.parentNode.tagName !== "BODY") {
+    //       currEl = currEl.parentNode
+    //     } else {
+    //       break
+    //     }
+    //   }
+
+    //   return siblingsArr;
+    // }
+
+    // console.log(domTreeTracer(1));
+    console.log(domTreeTracer(2));
+    // [["HEADER", "MAIN", "FOOTER"], ["ARTICLE"]]
+    console.log(domTreeTracer(20));
+    console.log(domTreeTracer(22));
+    // [["A"], ["STRONG"], ["SPAN", "SPAN"], ["P", "P"], ["SECTION", "SECTION"], ["HEADER", "MAIN", "FOOTER"], ["ARTICLE"]]
+})
+
 // function traceDom(num) {
 //     let result = [];
 //     let count = 0;
@@ -44,26 +90,26 @@
 
 // }
 
-window.onload = function() {
-    function domTreeTracer(id) {
-      let tree = [];
-      let theElement = document.getElementById(id);
+// window.onload = function() {
+//     function domTreeTracer(id) {
+//       let tree = [];
+//       let theElement = document.getElementById(id);
 
-      do {
-        let siblings = [];
+//       do {
+//         let siblings = [];
 
-        for (child of theElement.parentElement.children) {
-          siblings.push(child.tagName);
-        }
+//         for (child of theElement.parentElement.children) {
+//           siblings.push(child.tagName);
+//         }
 
-        tree.push(siblings);
-        theElement = theElement.parentElement;
-      } while (theElement !== document.body);
+//         tree.push(siblings);
+//         theElement = theElement.parentElement;
+//       } while (theElement !== document.body);
 
-      return tree;
-    }
+//       return tree;
+//     }
 
-    console.log(domTreeTracer(1));
-    console.log(domTreeTracer(2));
-    console.log(domTreeTracer(22));
-  }
+//     console.log(domTreeTracer(1));
+//     console.log(domTreeTracer(2));
+//     console.log(domTreeTracer(22));
+//   }
